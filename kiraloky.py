179bb5dcd9a7005299f16a5009712fce0a7f221a
bencevrkonyi <varkonyi.bence@diak.szbi-pg.hu>
2025-03-21 13:33:21 +0100

    Update kiraloky.py

--- kiraloky.py
+++ kiraloky.py
@@ -4,9 +4,27 @@ cnx = connection.MySQLConnection(user='root', password='',
                                  host='127.0.0.1',
                                  database='kiralyok')
 
+# táblák megjelenítése
 cursor = cnx.cursor()
 cursor.execute("SHOW TABLES")
 for table in cursor:
     print(table)
+
+# uralkodók megjelenítése
+cursor.execute("SELECT * FROM uralkodo")
+for uralkodo in cursor:
+    print(uralkodo)
+
+# mátyás király
+cursor.execute("SELECT * FROM uralkodo WHERE uralkodo.nev='I. Mátyás'")
+for uralkodo in cursor:
+    print(uralkodo)
+print("----------------------")
+
+# mátyás király
+cursor.execute("SELECT szul,hal FROM uralkodo WHERE uralkodo.nev='I. Mátyás")
+for uralkodo in cursor:
+    print(uralkodo)
+print("----------------------")
 
 cnx.close()
