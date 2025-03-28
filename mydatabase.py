784e5c82f05304dc35cfc375a1c7103e054db1c6
bencevrkonyi <varkonyi.bence@diak.szbi-pg.hu>
2025-03-28 13:21:12 +0100

    Update mydatabase.py

--- mydatabase.py
+++ mydatabase.py
@@ -8,13 +8,26 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
+# adatbázis létrehozása
 DATABASE = "mydatabase"
 
 mycursor.execute(f"CREATE DATABASE IF NOT EXISTS {DATABASE}")
 
+# adatbázisok mutatása
 mycursor.execute("SHOW DATABASES")
 
 for x in mycursor:
   print(x)
 
-mycursor.execute(f"USE {DATABASE}")  
+# adatbázis használata
+mycursor.execute(f"USE {DATABASE}")
+
+# táblázat létrehozása
+mycursor.execute("CREATE TABLE IF NOT EXISTS customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))")
+print("------------------------")
+
+# táblák mutatása
+mycursor.execute("SHOW TABLES")
+
+for x in mycursor:
+  print(x)
